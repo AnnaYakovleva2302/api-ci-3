4ba2a7bf023e623472ec7c0738521f9f150f50e1
Anna Yakovleva <_bel4onok_@mail.ru>
2021-11-13 23:19:22 +0300

    Add test for postman echo /post route

---
+++ src/test/java/ru/netology/rest/PostmanEchoApiTest.java
@@ -0,0 +1,20 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoApiTest {
+    @Test
+    void shouldReturnDemoBodyFromPostmanEcho() {
+        given()
+            .baseUri("https://postman-echo.com")
+            .contentType("text/plain; charset=UTF-8")
+            .body("Some Body Data")
+            .when()
+            .post("/post")
+            .then()
+            .statusCode(200)
+            .body("data", equalTo("Some Body Data"));
+    }
+}
